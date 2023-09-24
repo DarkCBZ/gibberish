245491e2c44b4ce877c8d098f79b9a5b2c97345f
DarkCBZ <91396838+DarkCBZ@users.noreply.github.com>
2023-09-24 13:45:28 -0500

    added site
    ez

---
+++ index.js
@@ -0,0 +1,43 @@
+let key = { a: "s", b: "n", c: "v", d: "f", e: "r", f: "g", g: "h", h: "j", i: "o", j: "k", k: "l", l: ";", m: ",", n: "m", o: "p", p: "[", q: "w", r: "t", s: "d", t: "y", u: "i", v: "b", w: "e", x: "c", y: "u", z: "x" };
+
+function englishToEncode(str) {
+  let encodedStr = "";
+  for (let i = 0; i < str.length; i++) {
+    let char = str.charAt(i);
+    encodedStr += key[char] || char;
+  }
+  return encodedStr;
+}
+
+function encodeToEnglish(str) {
+  let decodedStr = "";
+  for (let i = 0; i < str.length; i++) {
+    let char = str.charAt(i);
+    let translatedChar = null;
+    for (let encodedChar in key) {
+      if (key[encodedChar] === char) {
+        translatedChar = encodedChar;
+        break;
+      }
+    }
+    decodedStr += translatedChar || char;
+  }
+  return decodedStr;
+}
+
+const englishInput = document.getElementById("englishInput");
+const gibberishInput = document.getElementById("gibberishInput");
+const gibberishOutput = document.getElementById("gibberishOutput");
+const englishOutput = document.getElementById("englishOutput");
+
+englishInput.addEventListener("input", () => {
+  const inputText = englishInput.value;
+  const encodedText = englishToEncode(inputText);
+  gibberishOutput.value = encodedText;
+});
+
+gibberishInput.addEventListener("input", () => {
+  const inputText = gibberishInput.value;
+  const decodedText = encodeToEnglish(inputText);
+  englishOutput.value = decodedText;
+});
